198488bc253d717ab1d258e0d7df7cee626ba05f
Eric Betts <bettse@fastmail.fm>
2020-04-10 22:45:26 -0700

    Move source, add Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+
+let package = Package(
+    name: "amiitool",
+    products: [
+        .library(name: "amiitool", targets: ["amiitool"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/siddarthgandhi/mbedTLS.git", .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "amiitool",
+            path: "./Sources/amiitool"
+        ),
+    ]
+)
---
+++ Sources/amiitool/gitversion.h
@@ -0,0 +1,2 @@
+#define GIT_COMMIT_ID 0x4fe80a1d
+#define GIT_COMMIT_COUNT 59
